74cf7aed92605fba001b65ab1dc4a28c97adc5aa
dannyz2 <davez@dr.com>
2017-08-24 23:25:30 -0600

    Added Projects

---
+++ XC8Projects/16F720/20x4LCD_Parallel_Test.X/build/default/production/lcd_hd44780_pic16.p1.d
@@ -0,0 +1,14 @@
+ build/default/production/lcd_hd44780_pic16.d  \
+ build/default/production/lcd_hd44780_pic16.p1:  \
+ lcd_hd44780_pic16.c  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/xc.h  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/eeprom_routines.h  \
+lcd_hd44780_pic16.h  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/htc.h  \
+system.h  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/pic16f720.h  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/pic_chip_select.h  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/stdint.h  \
+myutils.h  \
+custom_char.h  \
+../../../Program\ Files\ (x86)/Microchip/xc8/v1.33/include/pic.h 
