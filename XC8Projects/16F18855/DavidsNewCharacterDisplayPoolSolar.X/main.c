ad5ca6f567424f7e781820d1bc4f05d4bb121b66
dannyz2 <davez@dr.com>
2018-02-20 13:49:37 -0600

    cnf

--- XC8Projects/16F18855/DavidsNewCharacterDisplayPoolSolar.X/main.c
+++ XC8Projects/16F18855/DavidsNewCharacterDisplayPoolSolar.X/main.c
@@ -9,6 +9,8 @@
 #define solarInTemp     analogs[4]
 #define solarOutTemp    analogs[5]
 
+
+
 // *************** Main Routine ************************************************    
 void main(void)
 {
@@ -18,7 +20,24 @@ void main(void)
     
     float displayTemp, displayTemp2;                    // Calculate R of Thermistor, and Temp using SteinHart/Hart equation
 
-    unsigned char x[8]      =   {0,64,2,0,0,0,0,10};       // looping var, cycle counter, tempDisplay, C or F autoDisplay, display intensity loop counter, startup delay, Reset delay to allow reset to occur before brightness adjustment occurs
+//    unsigned char x[8]      =   {0,64,2,0,0,0,0,10};       // looping var, cycle counter, tempDisplay, C or F autoDisplay, display intensity loop counter, startup delay, Reset delay to allow reset to occur before brightness adjustment occurs
+    
+    char x                  =   0;                      // Looping Initializer
+    
+    char loop               =   64;                     // cycle (loop) counter
+    
+    char choice             =   2;                      // Display degrees C or degrees F, or cycle through both if choice is "2".
+    
+    char C_or_F             =   1;                      // Default Temperature Display is degrees F
+    
+    unsigned int bCount     =   0;                      // Loop counter for Backlight setting delay (to allow selection of both pads touched)
+    
+    unsigned char startDelay=   0;                      // Loop counter to delay touchpad input until ADC stable
+    
+    unsigned char resetDelay=   0;                      // Loop counter to delay backlight touchpad input until determined if two pads are touched for Reset
+    
+    unsigned char tPadCount =   10;                     // Touch pad counter (counts up or down from 10, to determine is one or both pads touched) (for C_or_F choice))
+    
     
     uint16_t dutyCycle      = 1023;                     // display back light brightness
     
@@ -28,29 +47,57 @@ void main(void)
    
     LCD_Clear();
     
-    LCDWriteStringXY(2,1,"A");
+    LCDWriteStringXY(2,0,"A");
     __delay_ms(200);
-    LCDWriteStringXY(3,1,"L");
+    LCD_Write_String("L");
     __delay_ms(200);
-    LCDWriteStringXY(4,1,"O");
+    LCD_Write_String("O");
     __delay_ms(200);
-    LCDWriteStringXY(5,1,"H");
+    LCD_Write_String("H");
     __delay_ms(200);
-    LCDWriteStringXY(6,1,"A");
+    LCD_Write_String("A");
     __delay_ms(200);
-    LCDWriteStringXY(7,1," ");
+    LCD_Write_String(" ");
     __delay_ms(200);
-    LCDWriteStringXY(8,1,"D");
+    LCD_Write_String("D");
     __delay_ms(200);
-    LCDWriteStringXY(9,1,"A");
+    LCD_Write_String("A");
     __delay_ms(200);
-    LCDWriteStringXY(10,1,"V");
+    LCD_Write_String("V");
     __delay_ms(200);
-    LCDWriteStringXY(11,1,"I");
+    LCD_Write_String("I");
     __delay_ms(200);
-    LCDWriteStringXY(12,1,"D");
+    LCD_Write_String("D");
     __delay_ms(200);
-    LCDWriteStringXY(13,1,"!");
+    LCD_Write_String(",");
+    __delay_ms(200);
+    LCDWriteStringXY(1,1,"S");
+    __delay_ms(200);
+    LCD_Write_String("O");
+    __delay_ms(200);
+    LCD_Write_String("P");
+    __delay_ms(200);
+    LCD_Write_String("H");
+    __delay_ms(200);
+    LCD_Write_String("I");
+    __delay_ms(200);
+    LCD_Write_String("E");
+    __delay_ms(200);
+    LCD_Write_String(" ");
+    __delay_ms(200);
+    LCD_Write_String("&");
+    __delay_ms(200);
+    LCD_Write_String(" ");
+    __delay_ms(200);
+    LCD_Write_String("K");
+    __delay_ms(200);
+    LCD_Write_String("I");
+    __delay_ms(200);
+    LCD_Write_String("D");
+    __delay_ms(200);
+    LCD_Write_String("S");
+    __delay_ms(200);
+    LCD_Write_String("!");
     __delay_ms(5000);
     
     LCD_Clear();
@@ -60,54 +107,54 @@ void main(void)
 
     while (1)
     {
-//        LCDWriteIntXY(7,1,x[2],1,0,0);
-        for(x[0]=0;x[0]<6;x[0]++) analogs[x[0]]=readAnalog(x[0]);
+//        LCDWriteIntXY(7,1,choice,1,0,0);
+        for(x=0;x<6;x++) analogs[x]=readAnalog(x);
         
-        if(analogs[0]<1020)x[7]+=1;
+        if(analogs[0]<1020)tPadCount+=1;
             
             
-        if(analogs[1]<900)x[7]-=1;
+        if(analogs[1]<900)tPadCount-=1;
             
-        if(x[7]<1)
+        if(tPadCount<1)
         {
-            x[2]=1;
-            x[7]=10;
+            choice=1;
+            tPadCount=10;
         }
         
-        if(x[7]>19)
+        if(tPadCount>19)
         {
-            x[2]=0;
-            x[7]=10;
+            choice=0;
+            tPadCount=10;
         }
         
         if(analogs[0]<1020&&analogs[1]<970)
         {
-            x[2]=2;
+            choice=2;
             __delay_ms(200);
         }
         
         
         
-        x[5]+=1;
+        startDelay+=1;
         
-        if(x[5]>254)
+        if(startDelay>254)
         {
-            x[5]=254;
+            startDelay=254;
             
             if(down<900&&up<900)RESET();
             
             if(down<900)
             {
-                x[6]+=1;
+                resetDelay+=1;
                 
-                if(x[6]>20)
+                if(resetDelay>20)
                 {
                     while(down<975)
                     {
                         down=ADCRead(4);
                         
-                        if(x[4]<1)LCD_Clear();
-                        x[4]+=1;
+                        if(bCount<1)LCD_Clear();
+                        bCount+=1;
 
                         if(dutyCycle>0)dutyCycle-=1;
                 
@@ -117,25 +164,25 @@ void main(void)
                         LCD_Write_Char(' ');
                         PWM6_LoadDutyValue(dutyCycle);
                     }
-                    x[4]=0;
+                    bCount=0;
                     __delay_ms(100);
                     LCD_Clear();
-                    x[6]=0;
+                    resetDelay=0;
                 }
             }
 
             if(up<900)
             {
-                x[6]+=1;
+                resetDelay+=1;
                 
-                if(x[6]>20)
+                if(resetDelay>20)
                 {
                     while(up<975)
                     {
                         up=ADCRead(5);
 
-                        if(x[4]<1)LCD_Clear();
-                        x[4]+=1;
+                        if(bCount<1)LCD_Clear();
+                        bCount+=1;
 
                         if(dutyCycle<1009)dutyCycle+=1;
                 
@@ -145,10 +192,10 @@ void main(void)
                         LCD_Write_Char(' ');
                         PWM6_LoadDutyValue(dutyCycle);
                     }
-                    x[4]=0;
+                    bCount=0;
                     __delay_ms(100);
                     LCD_Clear();
-                    x[6]=0;
+                    resetDelay=0;
                 }
             }
         }
@@ -159,12 +206,12 @@ void main(void)
         
         displayTemp2 = tempCalc(solarOutTemp);
 
-        if(x[1]>127)
+        if(loop>127)
         {
-            LCDWriteStringXY(0,0,"Solar In");
-            LCDWriteStringXY(0,1,"Return");
+            LCDWriteStringXY(0,0,"In->Solar");
+            LCDWriteStringXY(0,1,"Out->Pool");
   
-            if(x[3]==0)
+            if(C_or_F==0)
             {
                 LCDWriteIntXY(9,0,(int)displayTemp,-1,1,0);
                 LCD_Write_Char(0);
@@ -177,7 +224,7 @@ void main(void)
                 LCD_Write_Char(' ');
             }
             
-            if(x[3]==1)
+            if(C_or_F==1)
             {
                 displayTemp = displayTemp*9/5+320;          // Display Temperature in DegF
                 displayTemp2 = displayTemp2*9/5+320;        // Display Temperature in DegF
@@ -194,11 +241,11 @@ void main(void)
             }
             
             
-            if(x[2]==0)x[3]=0;
+            if(choice==0)C_or_F=0;
 
-            if(x[2]==1)x[3]=1;
+            if(choice==1)C_or_F=1;
 
-            if(x[2]==2)x[3]=1-x[3];
+            if(choice==2)C_or_F=1-C_or_F;
 /*            LCDWriteIntXY(0,0,analogs[0],-1,0,0);
             LCD_Write_Char(' ');
             LCDWriteIntXY(6,0,analogs[1],-1,0,0);
@@ -214,8 +261,8 @@ void main(void)
             LCD_Write_Char(' ');
             
   */          
-            x[1]=0;
+            loop=0;
         }
-        x[1]+=1;
+        loop+=1;
     }
 }
